b21a747d56eb9c990dd44e8097d8790b595b0e69
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-07-10 01:09:35 +0300

    Update Instagram.js

--- commands/Instagram.js
+++ commands/Instagram.js
@@ -1,15 +1,15 @@
 const {cmd } = require('../lib')
 cmd({
-        pattern: "insta",
+        pattern: "انستا",
         desc: "download instagram post.",
         category: "downloader",
         filename: __filename
     },
     async(Void, citel,text,{isCreator}) => {
         const { Insta } = require('../lib')
-if(!text) return citel.reply('Need post url.')
+if(!text) return citel.reply('رابط البوست؟')
 let response = await Insta(text)
 for (let i=0;i<response.length;i++) {
-await Void.sendFileUrl(citel.chat, response[i], `*Downloaded Media from instagram.*`, citel)
+await Void.sendFileUrl(citel.chat, response[i], `*تم التحميل، تفضل استمتع*`, citel)
 }
     });
