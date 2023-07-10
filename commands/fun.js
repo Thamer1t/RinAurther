1e117edf8e6da3ba4f560d59f22cdafb48ec1ee6
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-07-10 05:17:04 +0300

    Update fun.js

--- commands/fun.js
+++ commands/fun.js
@@ -13,73 +13,9 @@ const { dare, truth, random_question } = require('../lib/truth-dare.js')
 const axios = require('axios')
 const { cmd } = require('../lib')
     //---------------------------------------------------------------------------
-cmd({
-            pattern: "question",
-            desc: "Random Question.",
-            category: "fun",
-            filename: __filename,
-        },
-        async(Void, citel, text) => {
-            return await citel.reply(`${random_question()}`);
-        }
-    )
-    //---------------------------------------------------------------------------
-cmd({
-            pattern: "truth",
-            desc: "truth and dare(truth game.).",
-            category: "fun",
-            filename: __filename,
-        },
-        async(Void, citel, text) => {
-            return await citel.reply(`${truth()}`);
-        }
-    )
-    //---------------------------------------------------------------------------
-cmd({
-            pattern: "dare",
-            desc: "truth and dare(dare game.).",
-            category: "fun",
-            filename: __filename,
-        },
-        async(Void, citel, text) => {
-            return await citel.reply(`${dare()}`);
-        }
-    )
-    //---------------------------------------------------------------------------
-cmd({
-        pattern: "fact",
-        desc: "Sends fact in chat.",
-        category: "fun",
-        filename: __filename,
-    },
-    async(Void, citel, text) => {
-        const { data } = await axios.get(`https://nekos.life/api/v2/fact`)
-        return citel.reply(`*Fact:* ${data.fact}\n\n*Powered by Secktor*`)   
-    }
-
-)
-    //---------------------------------------------------------------------------
+------------------------------------------------------------
     cmd({
-        pattern: "quotes",
-        desc: "Sends quotes in chat.",
-        category: "fun",
-        filename: __filename,
-    },
-    async(Void, citel, text) => {
-        var quoo = await axios.get(`https://favqs.com/api/qotd`)
-        const replyf = `
-╔════◇
-║ *🎗️Content:* ${quoo.data.quote.body}
-║ *👤Author:* ${quoo.data.quote.author}
-║    
-╚════════════╝ `
-return citel.reply(replyf)
-    }
-
-)
-    //---------------------------------------------------------------------------
-    cmd({
-        pattern: "define",
+        pattern: "عرف",
         desc: "urban dictionary.",
         category: "fun",
         filename: __filename,
@@ -93,7 +29,7 @@ return citel.reply(replyf)
             Example: ${data.list[0].example.replace(/\[/g, "").replace(/\]/g, "")}`
             return citel.reply(textt)
                     } catch {
-                        return citel.reply(`No result for ${text}`)
+                        return citel.reply(`لاتوجد نتائج${text}`)
                     }
     }
 )
