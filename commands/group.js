b8d2a5fb0aec8c5731c3e44b3e2bc5a1e3e0b510
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-07-13 03:41:52 +0300

    Update group.js

--- commands/group.js
+++ commands/group.js
@@ -556,7 +556,7 @@ cmd({
             try {
                 let users = citel.mentionedJid[0] ? citel.mentionedJid[0] : citel.quoted ? citel.quoted.sender : text.replace(/[^0-9]/g, "") + "@s.whatsapp.net";
                 if (!users) return;
-                await Void.groupParticipantsUpdate(citel.chat, [users], "promote");
+                await Void.groupParticipantsUpdate(citel.chat, [users], "ترقية");
             } catch {
                 //		citel.reply(tlang().botAdmin);
 
@@ -583,7 +583,7 @@ cmd({
             try {
                 let users = citel.mentionedJid[0] ? citel.mentionedJid[0] : citel.quoted ? citel.quoted.sender : text.replace(/[^0-9]/g, "") + "@s.whatsapp.net";
                 if (!users) return;
-                await Void.groupParticipantsUpdate(citel.chat, [users], "remove");
+                await Void.groupParticipantsUpdate(citel.chat, [users], "طرد");
             } catch {
                 //		citel.reply(tlang().botAdmin);
 
