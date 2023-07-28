f4f92e898101c638391290aef7fad6097f048864
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-07-28 10:37:19 +0300

    Update Telegraph.js

--- commands/Telegraph.js
+++ commands/Telegraph.js
@@ -10,7 +10,7 @@
  * @version 0.0.6
  **/
 //---------------------------------------------------------------------------
-const { cmd }   = require('../lib');
+/*const { cmd }   = require('../lib');
 const util = require('util');
 const axios = require('axios');
 cmd({
@@ -24,4 +24,4 @@ cmd({
 let { data } = await axios.get(`https://api.telegra.ph/createPage?access_token=d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722&title=Secktor-Md+Bot&author_name=SamPandey001&content=[%7B"tag":"p","children":["${a.replace(/ /g,'+')}"]%7D]&return_content=true`);
 return citel.reply(`*Paste created on telegraph*\nName:-${util.format(data.result.title)} \nUrl:- ${util.format(data.result.url)}`)
     }
-);
+);*/
