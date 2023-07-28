45ba8dbdf6e4054efbffcd5574fcd127568333cc
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-07-28 07:21:55 +0300

    Update economy.js

--- commands/economy.js
+++ commands/economy.js
@@ -325,27 +325,46 @@ return await citel.reply(`🍀اليوزر: ${citel.pushName}\n\n_🪙${balance.
      //---------------------------------------------------------------------------
 
 
- cmd({
-        pattern: "سرقة",
-        desc: "rob bank amount.",
-        category: "economy",
-        filename: __filename,
-    },
-    async(Void, citel, text,{ isCreator }) => {
-	      // Get the user ID of the person running the command
+function cdl(duration) {
+    const seconds = Math.floor(duration / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+
+    if (days > 0) {
+        return `${days} day${days > 1 ? 's' : ''}`;
+    } else if (hours > 0) {
+        return `${hours} hour${hours > 1 ? 's' : ''}`;
+    } else if (minutes > 0) {
+        return `${minutes} minute${minutes > 1 ? 's' : ''}`;
+    } else {
+        return `${seconds} second${seconds > 1 ? 's' : ''}`;
+    }
+}
+
+cmd({
+    pattern: "سرقة",
+    desc: "rob bank amount.",
+    category: "economy",
+    filename: __filename,
+},
+async (Void, citel, text, { isCreator }) => {
+    // Get the user ID of the person running the command
     const userId = citel.sender
 
     // Check if there's an hourly cooldown for this user
-    if (hourly.cd && hourly.cd[userId] && hourly.cd[userId] > Date.now()) {
+    if (hourlyCooldowns.has(userId)) {
         // Get the remaining time on the cooldown
-        const remainingTime = hourly.cd[userId] - Date.now()
+        const remainingTime = hourlyCooldowns.get(userId) - Date.now()
 
-        // Send a message indicating how long the user needs to wait
-        return citel.reply(`🫡 تعال بعد ${cdl(remainingTime)} لتسرق مرة أخرى.`)
+        // If the cooldown hasn't expired yet, send a message indicating how long the user needs to wait
+        if (remainingTime > 0) {
+            return citel.reply(`🫡 تعال بعد ${cdl(remainingTime)} لتسرق مرة أخرى.`)
+        }
     }
 
     // Set the hourly cooldown for this user
-    hourly.cd[userId] = Date.now() + hourlyCooldownTime
+    hourlyCooldowns.set(userId, Date.now() + hourlyCooldownTime)
 
     // Check if there's a regular cooldown for this user
     if (cooldowns.has(userId)) {
@@ -362,56 +381,50 @@ return await citel.reply(`🍀اليوزر: ${citel.pushName}\n\n_🪙${balance.
     cooldowns.set(userId, Date.now() + cooldownTime)
 
     // The rest of the command code goes here...
-        let zerogroup = (await sck.findOne({
-            id: citel.chat,
-        })) || (await new sck({
-                id: citel.chat,
-            })
-            .save());
-        let mongoschemas = zerogroup.economy || "false";
-        if (mongoschemas == "false") return citel.reply("*🚦Economy* is not active in current group.");
-        let users = citel.mentionedJid ? citel.mentionedJid[0] : citel.msg.contextInfo.participant || false;
-	if(!users) return citel.reply('منشن شخص تسرقه.')
-        const user1 = citel.sender
-        const user2 = users
-	const secktor = "secktor"
-	    const k = 1000
-        const balance1  = await eco.balance(user1, secktor)
-	const balance2  = await eco.balance(user2, secktor)
-	const typ = ['ran','rob','caught'];
+    let zerogroup = (await sck.findOne({
+        id: citel.chat,
+    })) || (await new sck({
+        id: citel.chat,
+    }).save());
+    let mongoschemas = zerogroup.economy || "false";
+    if (mongoschemas == "false") return citel.reply("🚦Economy is not active in current group.");
+    let users = citel.mentionedJid ? citel.mentionedJid[0] : citel.msg.contextInfo.participant || false;
+    if(!users) return citel.reply('منشن شخص تسرقه.')
+    const user1 = citel.sender
+    const user2 = users
+    const secktor = "secktor"
+    const k = 1000
+    const balance1 = await eco.balance(user1, secktor)
+    const balance2 = await eco.balance(user2, secktor)
+    const typ = ['ran','rob','caught'];
     const random = typ[Math.floor(Math.random() * typ.length)];
-    if (k > balance1.wallet) return citel.reply(`*☹️ ماعندك فلوس تدفع الغرامة اذا انمسكت، اترك السرقة لاهلها*`);
-    if (k > balance2.wallet) return citel.reply(`*ضحيتك طفرانة، اختر شخص معه فلوس وش تبي بالطفارى🫤.*`);
-    let tpy = random    
+    if (k > balance1.wallet) return citel.reply('*☹️ ماعندك فلوس تدفع الغرامة اذا انمسكت، اترك السرقة لاهلها*');
+    if (k > balance2.wallet) return citel.reply('*ضحيتك طفرانة، اختر شخص معه فلوس وش تبي بالطفارى🫤*');
+    let tpy = random
     switch (random) {
-       
         case 'ran':
-              await citel.reply(`*ضحيتك هرب، واضح انك مب يم السرقة حاول تغير مهنتك🫰.*`)
-              ////citel.react('🥹')
-
-              break
+            await citel.reply('*ضحيتك هرب، واضح انك مب يم السرقة حاول تغير مهنتك🫰.*')
+            ////citel.react('🥹')
+            break
         case 'rob':
-	  const deduff = Math.floor(Math.random() * 1000)	    
-          await eco.deduct(user2, secktor, deduff);
-          await eco.give(citel.sender, secktor, deduff);
-          await citel.reply(`*🤑 تم الزرف.🗡️*\nهربت ومعك ${deduff} في مخباك.`)
-          ////citel.react('💀')
-              break
+            const deduff = Math.floor(Math.random() * 1000)	    
+            await eco.deduct(user2, secktor, deduff);
+            await eco.give(citel.sender, secktor, deduff);
+            await citel.reply(`*🤑 تم الزرف.🗡️*\nهربت ومعك ${deduff} في مخباك.`)
+            ////citel.react('💀')
+            break
         case 'caught':
-           const rmoney = Math.floor(Math.random() * 1000)
-           await eco.deduct(user1, secktor, rmoney);
-           await citel.reply(`*مسكوك👮 الشرطة , وغرموك ${rmoney} 🪙 , معوض خير🥹.*`)
-           ////citel.react('😦')
-             break
-default:
- await citel.reply('*وش قاعد تسوي؟👀*.')
- //citel.react('🤔')
-
- }
-
+            const rmoney = Math.floor(Math.random() * 1000)
+            await eco.deduct(user1, secktor, rmoney);
+            await citel.reply(`*مسكوك👮 الشرطة , وغرموك ${rmoney} 🪙 , معوض خير🥹.*`)
+            ////citel.react('😦')
+            break
+        default:
+            await citel.reply('*وش قاعد تسوي؟👀*.')
+            //citel.react('🤔')
     }
-)
 
+});
      //---------------------------------------------------------------------------
      cmd({
         pattern: "سحب",
