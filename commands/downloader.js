b88624b6d089fb7d5b1f6be8a645bec2634cb932
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-07-28 09:34:25 +0300

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -198,7 +198,7 @@ async(Void, citel, text) => {
 cmd({
             pattern: "يوتيوب",
             desc: "Downloads video from youtube.",
-            category: "downloader",
+            category: "تحميل",
             filename: __filename,
             use: '<yt video url>',
         },
