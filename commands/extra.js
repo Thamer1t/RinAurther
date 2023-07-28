41d8476087e3a42ad67e410cbd1b47c2a71f3fd8
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-07-28 09:13:05 +0300

    Update extra.js

--- commands/extra.js
+++ commands/extra.js
@@ -15,14 +15,14 @@ const {cmd} = require('../lib')
 const PastebinAPI = require("pastebin-js");
 pastebin = new PastebinAPI("EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL");
 cmd({
-        pattern: "pastebin",
+        pattern: "رابط-كتابة",
         desc: "To check ping",
-        category: "extra",
+        category: "اضافي",
         filename: __filename,
     },
     async(Void, citel) => {
         if(!citel.quoted) return citel.reply('Please quote any text to get link.')
-        let data = await pastebin.createPaste(citel.quoted.text, "Secktor-Pastebin")
-        citel.reply('_Here is your link._\n'+data)
+        let data = await pastebin.createPaste(citel.quoted.text, "")
+        citel.reply('_تفضل كتابتك برابط._\n'+data)
     }
 );
