0255a361a8e4f76cf21f3b74cd452de794eb94b0
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-07-30 08:41:57 +0300

    Update _help.js

--- commands/_help.js
+++ commands/_help.js
@@ -53,12 +53,12 @@ async(Void, citel, text) => {
             .locale('id')
         const date = moment.tz('Asia/Riyadh').format('DD/MM/YYYY')
         let total = await sck1.countDocuments()
-        let str = `╭────《 ` + fancytext(Config.ownername.split(' ')[0], 58) + ` 》─────⊷\n`
+        let str = `───〔 ` + fancytext(Config.ownername.split(' ')[0], 58) + ` 〕───\n`
         str += '' + `\n` + ''
         for (const category in cmds) {
-            str += `*╭────〄 ${tiny(category)} 〄────╮* \n`;
+            str += `*╮*────〄 ${tiny(category)} 〄────*╭* \n`;
             if (text.toLowerCase() == category.toLowerCase()){
-                str = `*╭────〄 ${tiny(category)} 〄────╮* \n`;
+                str = `*╮*────〄 ${tiny(category)} 〄────*╭*  \n`;
                 for (const plugins of cmds[category]) {
                     str += `│ ${fancytext(plugins,1)} │\n`;
                 }
