6f7e59440ae3e28910b654593302bb008dcb9c4e
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-01 10:53:17 +0300

    Update كتابة.js

--- commands/كتابة.js
+++ commands/كتابة.js
@@ -25,7 +25,7 @@ cmd({
     const randomIndex = Math.floor(Math.random() * count);
     const anime = await AnimeName.findOne().skip(randomIndex);
 
-  citel.chat(`*${anime.name}*`);
+  citel.reply(`*${anime.name}*`);
   } catch (error) {
     console.error(error);
     citel.reply('حدث خطأ، حاول مجددا');
