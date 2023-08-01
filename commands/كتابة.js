11f35e67e92aee470335d97efedb138c6beccd17
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-01 09:04:14 +0300

    Create كتابة.js

---
+++ commands/كتابة.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const AnimeName = require('../lib/database/Animenames');
+
+// Replace the connection string with your own MongoDB URI
+const uri = 'mongodb+srv://<username>:<password>@<cluster>.mongodb.net/animeDB';
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Define the command to get a random anime name from the database
+cmd({
+  pattern: "كت",
+  desc: "Get a random anime name",
+  category: "ترفيه",
+  filename: __filename,
+}, async (match, citel) => {
+  try {
+    const count = await AnimeName.countDocuments();
+    const randomIndex = Math.floor(Math.random() * count);
+    const anime = await AnimeName.findOne().skip(randomIndex);
+
+    citel.reply(anime.name);
+  } catch (error) {
+    console.error(error);
+    citel.reply('حدث خطأ، حاول مجددا');
+  } finally {
+    mongoose.disconnect();
+  }
+});
